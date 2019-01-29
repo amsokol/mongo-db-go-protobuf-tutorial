bbd54e7d781a9375a9eea33c84301652c63938bb
Aleksandr Sokolovskii <github@amsokol.io>
2019-01-29 14:32:30 +0300

    Update protoc-gen.cmd

--- protoc-gen.cmd
+++ protoc-gen.cmd
@@ -4,6 +4,6 @@
 
 @protoc data.proto --proto_path=. --proto_path=third_party --go_out=plugins=grpc:.
 
-@protoc data.proto --proto_path=. --proto_path=third_party --gotag_out=xxx="bson+\"-\"",output_path=.":.
+@protoc data.proto --proto_path=. --proto_path=third_party --gotag_out=xxx="bson+\"-\"",output_path=.:.
 
 @echo Done
